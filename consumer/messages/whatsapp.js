ff0b297008480b10a6874cc99b122d529890d35f
romanoroei <roei1234@gmail.com>
2026-07-24 01:21:36 +0300

    Refresh consumer WhatsApp share preview and copy

--- consumer/messages/whatsapp.js
+++ consumer/messages/whatsapp.js
@@ -44,7 +44,7 @@ export function buildWhatsAppUrl(result, profile) {
 }
 
 export function buildShareMessage(url = SITE_CONFIG.publicBaseUrl) {
-  return `עצמאי? מצאתי בדיקה קצרה שעוזרת להבין כמה ניתן לשקול להפקיד לקרן ההשתלמות השנה ומה שווי הטבות המס האפשריות.\n\nהבדיקה ללא הרשמה וללא התחייבות:\n${url}`;
+  return `עצמאי? מצאתי בדיקה קצרה שעוזרת להבין כמה כדאי לך להפקיד לקרן ההשתלמות השנה ומה שווי הטבות המס האפשריות.\n\nהבדיקה ללא הרשמה וללא התחייבות:\n${url}`;
 }
 
 export function buildConsumerShareUrl(url = SITE_CONFIG.publicBaseUrl) {
