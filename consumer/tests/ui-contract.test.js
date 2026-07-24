a97d1cc6b271ea1d108f784b587d2ac59057adc3
romanoroei <roei1234@gmail.com>
2026-07-24 19:07:47 +0300

    Require cookie consent choice before continuing

--- consumer/tests/ui-contract.test.js
+++ consumer/tests/ui-contract.test.js
@@ -111,6 +111,7 @@ test('רכיבי הציות של האתר המקצועי זמינים בשני �
 
   for (const html of [landing, check]) {
     assert.match(html, /id="cookieNotice"/);
+    assert.match(html, /id="cookieNotice" role="dialog" aria-modal="true"/);
     assert.match(html, /אפשר לשפר את החוויה\?/);
     assert.match(html, /id="disclaimer"/);
     assert.match(html, /class="disclosure-modal"/);
